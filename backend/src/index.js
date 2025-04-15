8c065d5b211b11066bfb57d501c73e653393748a
Nathan Silva <nathansilva.dev@gmail.com>
2025-04-15 17:59:52 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,7 @@ const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 
-app.use(
-  cors({
-    origin: ["https://front-4-life.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", routes);
